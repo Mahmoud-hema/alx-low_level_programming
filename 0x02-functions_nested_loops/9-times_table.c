31cd5f53d3d29309a1972a913745911fe1faaa6d
Mahmoud-hema <mmm01095838936@gmail.com>
2023-03-21 19:18:58 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,6 +2,11 @@
 /**
 * times_table - prints the 9 times table, starting with 0
 */
+int main(void)
+{
+times_table();
+return (0);
+}
 void times_table(void)
 {
 int i, j, k;
@@ -31,9 +36,4 @@ _putchar((k % 10) + '0');
 }
 _putchar('\n');
 }
-}
-int main(void)
-{
-times_table();
-return (0);
 }
