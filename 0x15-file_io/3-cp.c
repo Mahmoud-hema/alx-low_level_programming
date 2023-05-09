0495ee73d8d10511a9b724f8f5c6f4cf800d27f9
Mahmoud-hema <mmm01095838936@gmail.com>
2023-05-09 10:06:39 -0700

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,8 +14,7 @@ char *b;
 b = malloc(sizeof(char) * 1024);
 if (b == NULL)
 {
-dprintf(STDERR_FILENO,
-"Error: Cannot write to the %s\n", f);
+dprintf(STDERR_FILENO, "Error: Cannot write to the %s\n", f);
 exit(99);
 }
 return (b);
@@ -38,45 +37,45 @@ exit(100);
 * mainc - Copy the content of any file to another file.
 * @argc: The number of arguments found to the program.
 * @argv: An array of pointers pointed to the arguments.
-* Return: 1 when success.
+* Return: 0 when success.
 * Description:exit code If the argument count is incorrect - 97.
-* Exit code if file_from is invalid or unable to be read - exit code 98.
-* Exit code If file_to cannot be created or written to - exit code 99.
-* Exit code If file_to or file_from cannot be closed - exit code 100.
+* if file_from is invalid or unable to be read - exit code 98.
+* If file_to cannot be created or written to - exit code 99.
+* If file_to or file_from cannot be closed - exit code 100.
 */
 int mainc(int argc, char *argv[])
 {
 int fr, t, a, x;
-char *y;
+char *b;
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-y = create_buffer(argv[2]);
+b = create_buffer(argv[2]);
 fr = open(argv[1], O_RDONLY);
-a = read(fr, y, 1024);
+a = read(fr, b, 1024);
 t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 do {
 if (fr == -1 || a == -1)
 {
 dprintf(STDERR_FILENO,
 "Error: Can't read from file %s\n", argv[1]);
-free(y);
+free(b);
 exit(98);
 }
-x = write(t, y, a);
+x = write(t, b, a);
 if (t == -1 || x == -1)
 {
 dprintf(STDERR_FILENO,
 "Error: Can't write to %s\n", argv[2]);
-free(y);
+free(b);
 exit(99);
 }
-a = read(fr, y, 1024);
+a = read(fr, b, 1024);
 t = open(argv[2], O_WRONLY | O_APPEND);
 } while (a > 0);
-free(y);
+free(b);
 cl_f(fr);
 cl_f(t);
 return (0);
